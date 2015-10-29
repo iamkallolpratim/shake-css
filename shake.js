543712dcefa0cd352e4c33ef210329c9103402e5
Kallol Pratim Saikia <ksaikia615@gmail.com>
2015-10-29 13:49:03 +0530

    Update shake.js

--- shake.js
+++ shake.js
@@ -1,1 +1,22 @@
 
+function shakeElement() {
+    $('#id_of_your_div_or_button').addClass('shake');
+    setTimeout(function() {
+        $('#id_of_your_div_or_button').removeClass('shake');
+    }, 3000);
+};
+
+//remove the setTimeout function to remove time interval
+
+//to shake the div or button every 5 seconds apply following script
+
+
+
+/*
+function shakeElement() {
+    $('#id_of_your_div_or_button').addClass('shake');
+    setInterval(function(){
+       $('#id_of_your_div_or_button').toggleClass('shake')
+    }, 3000);
+};
+*/
